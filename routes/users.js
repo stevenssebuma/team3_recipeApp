2e70595c62e5fe2265e58c5bfa05d8f975fb9b41
faharetana-rkt <rakotomaharitra@gmail.com>
2024-10-29 15:39:39 +0300

    added authenticate middleware

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const { isAuthenticated } = require('../middleware/authenticate.js')
 const usersController = require('../controllers/users.js');
+
 router.get('/', usersController.getAll);
 router.get('/:id', usersController.getSingle);
-router.post('/', usersController.createUser);
-router.put('/:id', usersController.updateUser);
-router.delete('/:id', usersController.deleteUser)
+router.post('/', isAuthenticated, usersController.createUser);
+router.put('/:id', isAuthenticated, usersController.updateUser);
+router.delete('/:id', isAuthenticated, usersController.deleteUser)
 
 module.exports = router;
