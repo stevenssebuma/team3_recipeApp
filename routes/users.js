0746f55e25a91c05d9fc0a510e9d927480287899
faharetana-rkt <rakotomaharitra@gmail.com>
2024-10-29 20:55:06 +0300

    modified isauthenticated to checkuser

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { isAuthenticated } = require('../middleware/authenticate.js')
+const { checkUser } = require('../middleware/authenticate.js')
 const usersController = require('../controllers/users.js');
 
 router.get('/', usersController.getAll);
 router.get('/:id', usersController.getSingle);
-router.post('/', isAuthenticated, usersController.createUser);
-router.put('/:id', isAuthenticated, usersController.updateUser);
-router.delete('/:id', isAuthenticated, usersController.deleteUser)
+router.post('/', checkUser, usersController.createUser);
+router.put('/:id', checkUser, usersController.updateUser);
+router.delete('/:id', checkUser, usersController.deleteUser)
 
 module.exports = router;
